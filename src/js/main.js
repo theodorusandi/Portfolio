b8383bffbb65fba6ad49a449a99275e45854fd4d
Andi <theodorusandi.g@gmail.com>
2024-10-24 18:39:18 -0400

    add styling

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+import linkedInLogoUrl from "../img/linkedInLogo.png"
+import gitHubLogoUrl from "../img/gitHubLogo.png"
+import hashnodeLogoUrl from "../img/hashnodeLogo.jpg"
+
+document.getElementById('img-linkedin-logo').src = linkedInLogoUrl
+document.getElementById('img-github-logo').src = gitHubLogoUrl
+document.getElementById('img-hashnode-logo').src = hashnodeLogoUrl
